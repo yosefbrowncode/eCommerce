7e6306274c7c02a6602d678e4a77b9844fecac60
Yosef Brown <yosef@aklaunch.com>
2022-09-04 22:56:54 +0300

    update

--- src/components/PictureComp.vue
+++ src/components/PictureComp.vue
@@ -11,8 +11,8 @@ export default {
         numForClass: 5,
         clickedBtn: null,
         showModal: false,
-        imageUrl: "src/assets/images/image-product-1.jpg",
-        image: ["src/assets/images/image-product-1.jpg","src/assets/images/image-product-2.jpg","src/assets/images/image-product-3.jpg","src/assets/images/image-product-4.jpg"]
+        imageUrl: "../assets/images/image-product-1.jpg",
+        image: ["../assets/images/image-product-1.jpg","../assets/images/image-product-2.jpg","../assets/images/image-product-3.jpg","../assets/images/image-product-4.jpg"]
     }
     },
     methods: {
